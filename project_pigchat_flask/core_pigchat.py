a61980eeb9f041d7c33a347ee83d9099b3754cd6
bochdu <dbc_924@outlook.com>
2025-03-08 21:51:59 +0800

    feature(PigChat Server):增加http服务端接口

---
+++ project_pigchat_flask/core_pigchat.py
@@ -0,0 +1,33 @@
+import pigemoji
+import core_pignum
+
+# PIG NUM
+# utf8_str - hex_str - emoji_str
+
+def utf8_to_emoji(utf8_str,timestamp,password):
+    if pigemoji.check_no_emoji_string(utf8_str):
+        hex_str = core_pignum.utf8_to_pignum(utf8_str,timestamp,password)
+        emoji_str = pigemoji.hex_to_boar_emoji(hex_str)
+    else:
+        emoji_str = utf8_str
+    return emoji_str
+
+def emoji_to_utf8(emoji_str,timestamp,password):
+    if pigemoji.check_emoji_string(emoji_str):
+        hex_str = pigemoji.boar_emoji_to_hex(emoji_str)
+        utf8_str = core_pignum.pignum_to_utf8(hex_str,timestamp,password)
+    else:
+        utf8_str = emoji_str
+    return utf8_str
+
+
+# test
+# utf8_string = '将军'
+# timestamp = 1741208586
+# password = "my_password"
+
+# emoji_str = utf8_to_emoji(utf8_string,timestamp,password)
+# print(emoji_str)
+
+# converted_utf8_string = emoji_to_utf8(emoji_str,timestamp,password)
+# print(converted_utf8_string)
