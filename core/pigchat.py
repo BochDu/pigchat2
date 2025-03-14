6569d421df6e368e488d74ec0c1e4ed4d33f1727
quaeast <424044876@qq.com>
2025-03-15 00:11:50 +0800

    feat: shadow

--- core/pigchat.py
+++ core/pigchat.py
@@ -1,16 +1,16 @@
-from . import pigemoji
+from . import pigshadow
 from . import pignum
+
 
 # PIG NUM
 # utf8_str - hex_str - emoji_str
 
 def determine_encryption_decryption(str):
-    if pigemoji.check_no_emoji_string(str):
-        return 'encrypt'
-    elif pigemoji.check_emoji_string(str):
+    if pigshadow.is_shadow(str):
         return 'decrypt'
     else:
-        return ''
+        return 'encrypt'
+
 
 def is_utf8_encoded(utf8_str):
     try:
@@ -19,18 +19,24 @@ def is_utf8_encoded(utf8_str):
     except UnicodeEncodeError:
         return False
 
-def utf8_to_emoji(utf8_str,timestamp,password):
+
+def utf8_to_emoji(utf8_str, timestamp, password):
     if is_utf8_encoded(utf8_str):
-        hex_str = pignum.utf8_to_pignum(utf8_str,timestamp,password)
-        emoji_str = pigemoji.hex_to_boar_emoji(hex_str)
+        hex_str = pignum.utf8_to_pignum(utf8_str, timestamp, password)
+        emoji_str = pigshadow.hex_to_shadow(hex_str)
     else:
         emoji_str = ''
     return emoji_str
 
-def emoji_to_utf8(emoji_str,timestamp,password):
-    if pigemoji.check_emoji_string(emoji_str):
-        hex_str = pigemoji.boar_emoji_to_hex(emoji_str)
-        utf8_str = pignum.pignum_to_utf8(hex_str,timestamp,password)
+
+def emoji_to_utf8(emoji_str, timestamp, password):
+    if pigshadow.is_shadow(emoji_str):
+        hex_str = pigshadow.shadow_to_hex(emoji_str)
+        utf8_str = pignum.pignum_to_utf8(hex_str, timestamp, password)
     else:
         utf8_str = emoji_str
     return utf8_str
+
+
+
+
